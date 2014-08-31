362ea26f8d39cd9d942d65ba9edd87fbd0a2ee58
kevin mc inerney <kevinmcinerney8@gmail.com>
2014-08-31 13:29:33 -0700

    new commit

--- tweemo/urls.py
+++ tweemo/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^all/$', 'tweemo.views.home'),
-    url(r'^temp_test/$', 'tweemo.views.temp_test'),
+    url(r'^$', 'tweemo.views.home'),
     url(r'^tweets/$', 'tweemo.views.tweets'),
     url(r'^contactus/$', 'tweemo.views.contactus'),
     url(r'^aboutus/$', 'tweemo.views.aboutus'),
     url(r'^results/$', 'tweemo.views.results'),
-    url(r'^stream/$', 'tweemo.views.stream'),
     url(r'^thankyou/$', 'tweemo.views.thankyou'),
     url(r'^gallary/$', 'tweemo.views.gallary'),
     url(r'^usingtweemo/$', 'tweemo.views.usingtweemo'),
